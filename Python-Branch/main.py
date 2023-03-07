7b5f352684c6d16540cb996f417c2de686f0a3ca
Sir-Cleetus <98899989+Sir-Cleetus@users.noreply.github.com>
2023-03-07 14:38:30 +0000

    Potential-Login-System && Possible Template

--- Python-Branch/main.py
+++ Python-Branch/main.py
@@ -1,9 +1,28 @@
-# Imports 
-import tkinter
+import customtkinter
 
-# Variables
-root = tkinter.Tk()
-label = tkinter.Label(root)
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
-# Code
-label.pack()
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Test")
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Test")
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Test")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
