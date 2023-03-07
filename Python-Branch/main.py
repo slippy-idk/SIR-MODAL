4ec15d13610d18bcd42efb901211230c09230b28
Sir-Cleetus <98899989+Sir-Cleetus@users.noreply.github.com>
2023-03-07 14:47:50 +0000

    GUI Variable Titling

--- Python-Branch/main.py
+++ Python-Branch/main.py
@@ -4,25 +4,28 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
+root.title("Sir Model/Login")
 root.geometry("500x350")
 
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Test")
+label = customtkinter.CTkLabel(master=frame, text="Login Screen")
 label.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
-entry1.pack(pady=12, padx=10)
+Username = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+Username.pack(pady=12, padx=10)
+Password = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+Password.pack(pady=12, padx=10)
 
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
-entry2.pack(pady=12, padx=10)
+login_Button = customtkinter.CTkButton(master=frame, text="Login")
+login_Button.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Test")
-button.pack(pady=12, padx=10)
+signup_Button = customtkinter.CTkButton(master=frame, text="Signup")
+signup_Button.pack(pady=12, padx=10)
 
-checkbox = customtkinter.CTkCheckBox(master=frame, text="Test")
-checkbox.pack(pady=12, padx=10)
+terms_Checkbox = customtkinter.CTkCheckBox(master=frame, text="I agree to the terms and conditions of use")
+terms_Checkbox.pack(pady=12, padx=10)
 
 root.mainloop()
