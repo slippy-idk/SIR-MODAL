9b80f3e1c448a9e7f03853926fe906ecdc2a97d5
slippy <60573072+slippy-idk@users.noreply.github.com>
2023-04-03 00:36:30 +0100

    Complete change of SIR modal
    the inital modal was flawed and giving arenious results so i have overhauled with the while now incomplete more fundemtly sound version of the modal while better at adhering to the SIR equation

--- Python-Branch/SIR/Main.py
+++ Python-Branch/SIR/Main.py
@@ -1,49 +1,51 @@
-#variables relating to the virus itself 
-Infectivity = int(input("Please eneter the innfectivity rate of the modal virus"))
-Recovery = int(input("please enter the mean revery rate of a virus"))
+day = 1
+S = 80
+I = 20
+R = 0
+pop = 100
 
-#variables relating to the current states of the simnulated population
-susetible = 1000
-recovered = 0
-Innfected = 1
-susetible = susetible - Innfected  #might be redunent
-pop = susetible + Innfected #this variable is the current population of the twon worked out may work out as might be redunent
-day = 0  # the currfent day
+ST = 0
 
+TI = 0
 
-#thease varibels are unused int he current versin but may be used lated for user simplicity
-Rday = 1/Recovery
-caseinf = Infectivity/Recovery
+TR = 0
 
-simdays = int(input("How many days would you like to simulate"))
+inf_rate = 2
+inf_length = 4
 
-def sussy(): #below is the function that simulates the infection of a day by shwing the change in suseptible 
-    global susetible
-    global suseptible
-    global Innfected
-    force_inf = Infectivity*Innfected
-    force_inf = force_inf*pop
-    force_inf = force_inf/pop
-    force_inf = round(force_inf)
-    print("current change of susetible pop -" + str(force_inf))
-    Innfected = Innfected + force_inf
-    susetible = susetible - force_inf
-    print("the current innfected pop " + str(Innfected))
-    
-def amon():
-    global Recovery
-    global susetible
-    global Innfected
-    norm = Infectivity*Innfected
-    norm = norm*pop
-    norm = round(norm)
-    force_rec = Recovery*Innfected
-    force_rec = force_rec - norm
-    print("change of reocovery " + str(force_rec))
-
-amon()
-    
+rec_rate = 1.0 / inf_length
 
 
 
+def sus():
+    global S
+    global ST
+    ST = -inf_rate*S*I/ pop
+    print(ST)
+
+def inf():
+    global I
+    global TI
+    TI = inf_rate*S*I/ pop
+    print(TI)
+
+def rec():
+    global R
+    global TR
+    TR = rec_rate * I
+    print(TR)
+
+
+def change():
+    global TR
+    global TI
+    global ST
+    global S
+    global I
+    global R
+    S = S + ST
+    print("Susetible = " + str(S))
+
+sus()
+change()
 
