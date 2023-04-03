a39445a387a9c588b2e10dbdaf26385717dfd20a
slippy <60573072+slippy-idk@users.noreply.github.com>
2023-04-03 18:07:10 +0100

    Full working model
    this is the full basic sir modal that works with day simulation causing a completion of our main task int he design doc

--- Python-Branch/SIR/Main.py
+++ Python-Branch/SIR/Main.py
@@ -1,39 +1,40 @@
+sim_day = int(input("please enter the amount of days you wish to simulate")
 day = 1
-S = 80
-I = 20
+S = int(input("please enter your susetible population"))
+I = int(input("please neter your infected population"))
 R = 0
-pop = 100
+pop = S + I + R
 
-ST = 0
-
+ST = 0 # need to declere the three variables below so they become global beforehand
+#they all focous around the change of the modal pop for that group the next day 
 TI = 0
 
 TR = 0
 
-inf_rate = 2
-inf_length = 4
+inf_rate = int(input("please enter the amount of people a day the infected cause the infection to spread"))
+inf_length = int(input("please enter the amount of days the infection lasts"))
 
 rec_rate = 1.0 / inf_length
 
 
 
-def sus():
+def sus(): # work out change of susetible pop
     global S
     global ST
     ST = -inf_rate*S*I/ pop
-    print(ST)
+    inf()
 
 def inf():
     global I
     global TI
     TI = inf_rate*S*I/ pop
-    print(TI)
+    rec()
 
 def rec():
     global R
     global TR
     TR = rec_rate * I
-    print(TR)
+    change()
 
 
 def change():
@@ -42,10 +43,27 @@ def change():
     global ST
     global S
     global I
+    global day
     global R
     S = S + ST
+    I = I + TI
+    R = R + TR
     print("Susetible = " + str(S))
+    print("Infected = " + str(I))
+    print("Recovered = " + str(R))
+    day = day + 1
+    wam()
 
-sus()
-change()
+def test():
+    print("a")
 
+def wam():
+    global day
+    print(day)
+    if day > sim_day:
+        print("l")
+    else:
+        sus()
+        
+wam()
+    
